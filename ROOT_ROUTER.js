0eab92e2d50e7faf7efaf08706603b1592c15c5a
ev-dev <evdev.js@gmail.com>
2017-10-12 15:48:09 +0000

    update to www for each proxy

--- ROOT_ROUTER.js
+++ ROOT_ROUTER.js
@@ -5,5 +5,7 @@ proxy.register('ev-dev.com', 'http://ev-dev.com:8000')
 proxy.register('www.ev-dev.com', 'http://ev-dev.com:8000')
 
 proxy.register('examplejs.ev-dev.com', 'http://ev-dev.com:8001')
+proxy.register('www.examplejs.ev-dev.com', 'http://ev-dev.com:8001')
 
 proxy.register('typeless.ev-dev.com', 'http://ev-dev.com:8002')
+proxy.register('www.typeless.ev-dev.com', 'http://ev-dev.com:8002')
