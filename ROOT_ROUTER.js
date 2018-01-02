f849376ca257dc970624c296cdfb554cd4352ab4
ev-dev <evdev.js@gmail.com>
2018-01-02 22:15:53 +0000

    Update to reverse proxy domains

--- ROOT_ROUTER.js
+++ ROOT_ROUTER.js
@@ -12,3 +12,9 @@ proxy.register('www.typeless.ev-dev.com', 'http://ev-dev.com:8002')
 
 proxy.register('consilience.ev-dev.com', 'http://ev-dev.com:8003')
 proxy.register('www.consilience.ev-dev.com', 'http://ev-dev.com:8003')
+
+proxy.register('starter.ev-dev.com', 'http://ev-dev.com:3000')
+proxy.register('www.starter.ev-dev.com', 'http://ev-dev.com:300')
+
+proxy.register('testing.ev-dev.com', 'http://ev-dev.com:8005')
+proxy.register('www.testing.ev-dev.com', 'http://ev-dev.com:8005')
