91191922e166bbfc6151d909e4e28498f21c1f2d
Ubuntu <evdev@prod-vm.t3vc1jrrbgierb5cumhm1vuhth.bx.internal.cloudapp.net>
2018-01-03 01:37:19 +0000

    updated proxy domains to be from localhost instead of making a seperate http request to the same domain...duh

--- ROOT_ROUTER.js
+++ ROOT_ROUTER.js
@@ -1,17 +1,17 @@
 const proxy = require('redbird')({ port: 80 })
 
 // DEFAULT subdomain running Portfolio
-proxy.register('ev-dev.com', 'http://ev-dev.com:8000')
-proxy.register('www.ev-dev.com', 'http://ev-dev.com:8000')
+proxy.register('ev-dev.com', 'http://127.0.0.1:8000')
+proxy.register('www.ev-dev.com', 'http://127.0.0.1:8000')
 
-proxy.register('examplejs.ev-dev.com', 'http://ev-dev.com:8001')
-proxy.register('www.examplejs.ev-dev.com', 'http://ev-dev.com:8001')
+proxy.register('examplejs.ev-dev.com', 'http://127.0.0.1:8001')
+proxy.register('www.examplejs.ev-dev.com', 'http://127.0.0.1:8001')
 
-proxy.register('typeless.ev-dev.com', 'http://ev-dev.com:8002')
-proxy.register('www.typeless.ev-dev.com', 'http://ev-dev.com:8002')
+proxy.register('typeless.ev-dev.com', 'http://127.0.0.1:8002')
+proxy.register('www.typeless.ev-dev.com', 'http://127.0.0.1:8002')
 
-proxy.register('consilience.ev-dev.com', 'http://ev-dev.com:8003')
-proxy.register('www.consilience.ev-dev.com', 'http://ev-dev.com:8003')
+proxy.register('consilience.ev-dev.com', 'http://127.0.0.1:8003')
+proxy.register('www.consilience.ev-dev.com', 'http://127.0.0.1:8003')
 
 proxy.register('starter.ev-dev.com', 'http://ev-dev.com:3000')
 proxy.register('www.starter.ev-dev.com', 'http://ev-dev.com:300')
